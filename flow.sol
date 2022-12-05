69feff79c241ad7d1159038d0c18cb165fcce5a9
marcopedrinazzi <marcopedrinazzi.mp@gmail.com>
2022-12-05 11:43:41 +0100

    comments p1

--- flow.sol
+++ flow.sol
@@ -4,19 +4,27 @@ pragma solidity ^0.8.7;
 
 import "@chainlink/contracts/src/v0.8/ChainlinkClient.sol";
 import "@chainlink/contracts/src/v0.8/ConfirmedOwner.sol";
-import "./evidenceTypeDeclaration.sol";
-import "./certModelDeclaration.sol";
+import "./evidenceTypeDeclaration.sol"; //declaration of evidenceType struct
+import "./certModelDeclaration.sol"; //declaration of certModel struct
 
 contract Certificate{
 
     struct cert_type{  
         address certmodel_addr;
         evidenceType[] evidence;
-        //metrics - HOW CAN I ENCODE THEM? SHOULD WE OMIT THEM RIGHT NOW?
+        //metrics - HOW CAN I ENCODE THEM? SHOULD WE OMIT THEM RIGHT NOW (considering the PoC)?
     }
     
-    cert_type public cert;
+    cert_type public cert; //state variable (so memorized in the blockchain) that stores the certification data
 
+    /*Constructor: when i iniate the smart contract, the certificate state variable is initialized with the certification data.
+    The constructor takes as a parameter the certification model to "link" it to the certificate and to get the needed information:
+    - the certification model address 
+    - the evidences
+    (- the metrics (?))
+    
+    To populate the certificate transactions are executed.
+    */
     constructor(CertificationModel m){
         cert.certmodel_addr= m.getCertModelAddress();
         for (uint i = 0; i < m.SIZE(); i++) {
@@ -24,26 +32,27 @@ contract Certificate{
             cert.evidence[i].output = m.getEvidenceOutput(i);
             cert.evidence[i].result = m.getEvidenceResult(i);
         }
+        //metrics??
     }
 
 }
 
 contract CertificationModel{
 
-    certModel public model;
+    certModel public model; //state variable contianing the 
     uint public constant SIZE = 1; //size of the evidence collection model
-    evidenceType[SIZE] public evidence; 
-    event testResult(bool res);
+    evidenceType[SIZE] public evidence; //state variable, an array to store the evidence of the tests
 
     constructor(string memory _non_functional_property, string memory _target_of_certification, address _oracleAddr){
-        model.non_functional_property = _non_functional_property;
-        model.target_of_certification = _target_of_certification;
+        model.non_functional_property = _non_functional_property; //init non functional property
+        model.target_of_certification = _target_of_certification; //init target of certification
         model.evidence_collection_model[0]=test1;
         //evaluation function
         model.certModelAddr = address(this);
         model.oracleAddr = _oracleAddr;
     }
 
+    //this function executes the certification model
     function run() public {
         model.evidence_collection_model[0]();
     }
@@ -55,7 +64,6 @@ contract CertificationModel{
 
     function collectEvidenceTest1() public {
         APIConsumer api = APIConsumer(model.oracleAddr);
-        emit testResult(api.result());
         if(api.result() == true){
             evidence[0].testName = "test1";
             evidence[0].output = api.result();
@@ -68,6 +76,7 @@ contract CertificationModel{
         }
     }
 
+    //get functions needed to create and deploy the certificate smart contract in the following phases
     function getEvidenceTestName(uint index) public view returns(string memory){
         return evidence[index].testName;
     }
@@ -129,14 +138,16 @@ contract CertificationExecutionAndAward {
     
 }
 
+//Oracle - ChainLink
 contract APIConsumer is ChainlinkClient, ConfirmedOwner {
 
     using Chainlink for Chainlink.Request;
-    bool public result;
+    bool public result; //result of the API call
     bytes32 private jobId;
     uint256 private fee;
     event RequestCompleted(bytes32 indexed requestId, bool result);
 
+    // Initialize the link token and target oracle (https://docs.chain.link/any-api/testnet-oracles/)
     constructor() ConfirmedOwner(msg.sender) {
         setChainlinkToken(0x326C977E6efc84E512bB9C30f76E30c160eD06FB);
         setChainlinkOracle(0xCC79157eb46F5624204f47AB42b3906cAA40eaB7);
@@ -145,6 +156,9 @@ contract APIConsumer is ChainlinkClient, ConfirmedOwner {
     }
 
 
+    /* Create a Chainlink request to retrieve API response, find the target
+    data (completed field) (the expected behavior of the test is that completed is true).
+    */
     function requestCompletedData() public returns (bytes32 requestId) {
         Chainlink.Request memory req = buildChainlinkRequest(jobId, address(this), this.fulfill.selector);
         req.add("get", "http://jsonplaceholder.typicode.com/todos/4");
@@ -152,13 +166,13 @@ contract APIConsumer is ChainlinkClient, ConfirmedOwner {
         return sendChainlinkRequest(req, fee);
     }
 
-
+    //Receive the response in the form of bool
     function fulfill(bytes32 _requestId, bool _result) public recordChainlinkFulfillment(_requestId) {
         emit RequestCompleted(_requestId, _result);
         result = _result;
     }
 
-
+    //Allow withdraw of Link tokens from the contract
     function withdrawLink() public onlyOwner {
         LinkTokenInterface link = LinkTokenInterface(chainlinkTokenAddress());
         require(link.transfer(msg.sender, link.balanceOf(address(this))), "Unable to transfer");
