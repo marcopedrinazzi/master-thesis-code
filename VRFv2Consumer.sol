ef9010540f20d40954b269f2051669aa57e4d3e3
marcopedrinazzi <marcopedrinazzi.mp@gmail.com>
2022-12-13 11:36:59 +0100

    edit random

--- VRFv2Consumer.sol
+++ VRFv2Consumer.sol
@@ -7,14 +7,7 @@ import "@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol";
 import "@chainlink/contracts/src/v0.8/ConfirmedOwner.sol";
 
 /**
- * Request testnet LINK and ETH here: https://faucets.chain.link/
- * Find information on LINK Token Contracts and get the latest ETH and LINK faucets here: https://docs.chain.link/docs/link-token-contracts/
- */
-
-/**
- * THIS IS AN EXAMPLE CONTRACT THAT USES HARDCODED VALUES FOR CLARITY.
- * THIS IS AN EXAMPLE CONTRACT THAT USES UN-AUDITED CODE.
- * DO NOT USE THIS CODE IN PRODUCTION.
+    https://vrf.chain.link/goerli/7589
  */
 
 contract VRFv2Consumer is VRFConsumerBaseV2, ConfirmedOwner {
